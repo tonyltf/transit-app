97cf29d710a03b81381a94065e7a716137ececd8
Tony Li <tingfung.tony@gmail.com>
2023-06-24 03:15:30 +0800

    🚀 feat(package.json): add new dependencies for emotion, mantine and notifications 🎨 style(layout.tsx): change metadata title and description to match the app name 🎨 style(layout.tsx): add ThemeProvider component to wrap NextAuthProvider 🎨 style(providers.tsx): add ThemeProvider component to provide MantineProvider with a dark color scheme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { SessionProvider } from 'next-auth/react'
+import { MantineProvider } from '@mantine/core'
 
 type Props = { children: React.ReactNode }
 
@@ -9,5 +10,19 @@ export const NextAuthProvider = ({ children }: Props) => {
     <SessionProvider>
       {children}
     </SessionProvider>
+  )
+}
+
+export const ThemeProvider = ({ children }: Props) => {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCss
+      theme={{
+        colorScheme: 'dark',
+      }}
+    >
+      {children}
+    </MantineProvider>
   )
 }
