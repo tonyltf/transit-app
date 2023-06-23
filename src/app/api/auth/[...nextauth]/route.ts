69f8ce83f510e536c4b8d47624f39dfa508c843b
Tony Li <tingfung.tony@gmail.com>
2023-06-24 02:50:40 +0800

    🐛 fix(route.ts): import authOptions from lib/auth instead of GoogleProvider ✨ feat(page.tsx): add conditional rendering for sign in and sign out buttons based on session ✨ feat(providers.tsx): create NextAuthProvider component to wrap SessionProvider around children
    🎉 feat(auth.ts): add GoogleProvider to NextAuthOptions with clientId and clientSecret from environment variables

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,7 @@
+import { authOptions } from '@/lib/auth'
 import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
 
-const handler = NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!
-    }),
-  ],
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
 
---
+++ src/app/login/buttons.component.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { signIn, signOut } from 'next-auth/react'
+
+export const GoogleSignInButton = () => {
+  return <button className="p-4 m-4 text-white bg-blue-500 rounded-lg" onClick={() => signIn('google')}>Sign In</button>
+}
+
+export const SignOutButton = () => {
+  return <button className="p-4 m-4 text-white bg-blue-500 rounded-lg" onClick={() => signOut()}>Sign Out</button>
+}
+
