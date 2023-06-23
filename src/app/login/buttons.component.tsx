3906dac6ad399350a7a0bc2a824fd15cea25fea1
Tony Li <tingfung.tony@gmail.com>
2023-06-24 03:20:44 +0800

    🎨 style(layout.tsx): remove trailing newline 🎨 style(buttons.component.tsx): add 'with Google' to Google sign in button text

--- src/app/login/buttons.component.tsx
+++ src/app/login/buttons.component.tsx
@@ -3,7 +3,7 @@
 import { signIn, signOut } from 'next-auth/react'
 
 export const GoogleSignInButton = () => {
-  return <button className="p-4 m-4 text-white bg-blue-500 rounded-lg" onClick={() => signIn('google')}>Sign In</button>
+  return <button className="p-4 m-4 text-white bg-blue-500 rounded-lg" onClick={() => signIn('google')}>Sign In with Google</button>
 }
 
 export const SignOutButton = () => {
